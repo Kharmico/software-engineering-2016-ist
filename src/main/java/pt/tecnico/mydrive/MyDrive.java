5c488ec0bf491dfa9616bab378744ee9bb3d978b
Francisco Santos <franciscopolaco@tecnico.ulisboa.pt>
2016-02-23 16:55:44 +0000

    Pom atualizado, class de teste feita

--- src/main/java/pt/tecnico/mydrive/MyDrive.java
+++ src/main/java/pt/tecnico/mydrive/MyDrive.java
@@ -1,6 +1,8 @@
+package pt.tecnico.mydrive;
+
 public class MyDrive{
 
-	public void static main(String[] args){
+	public static void main(String[] args){
 		System.out.println("Estou vivo");
 	}
 }
