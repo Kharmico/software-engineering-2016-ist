fea21735cc0913d50b4c0d89850cade942489e3f
Francisco Santos <franciscopolaco@ist.utl.pt>
2016-03-01 22:00:01 +0000

    Normalizacao dos nomes dos ficheiros e da class Main

--- src/main/java/pt/tecnico/myDrive/Main.java
+++ src/main/java/pt/tecnico/myDrive/Main.java
@@ -1,8 +1,8 @@
-package pt.tecnico.mydrive;
+package pt.tecnico.myDrive;
 
-public class MyDrive{
+public class Main{
 
 	public static void main(String[] args){
-		System.out.println("Estou vivo");
+		System.out.println("Hello World!");
 	}
 }
