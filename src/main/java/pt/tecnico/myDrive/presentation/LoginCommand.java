4d05e8ab1bead64f8248255fa71864084ab70aeb
drlferreira <diogo.lopes.ferreira@tecnico.ulisboa.pt>
2016-04-24 14:06:12 +0100

    Now MdCommands have the token included

--- src/main/java/pt/tecnico/myDrive/presentation/LoginCommand.java
+++ src/main/java/pt/tecnico/myDrive/presentation/LoginCommand.java
@@ -1,10 +1,9 @@
 package pt.tecnico.myDrive.presentation;
 
+import pt.tecnico.myDrive.domain.MyDriveManager;
 import pt.tecnico.myDrive.service.LoginUserService;
 
 public class LoginCommand extends MdCommand {
-
-    private long _returnedToken;
 
     public LoginCommand(Shell sh) { super(sh, "login", "does the login service"); }
 
@@ -15,12 +14,8 @@ public class LoginCommand extends MdCommand {
         else {
             LoginUserService lus = args.length == 1 ? new LoginUserService(args[0], "") : new LoginUserService(args[0], args[1]);
             lus.execute();
-            _returnedToken = lus.result();
+            setToken(lus.result());
         }
-    }
-
-    public long getReturnedToken(){
-        return _returnedToken;
     }
 
 }
--- src/main/java/pt/tecnico/myDrive/presentation/MdCommand.java
+++ src/main/java/pt/tecnico/myDrive/presentation/MdCommand.java
@@ -1,9 +1,21 @@
 package pt.tecnico.myDrive.presentation;
 
+import pt.tecnico.myDrive.service.LoginUserService;
+
 /**
  * Created by xxlxpto on 23-04-2016.
  */
 public abstract class MdCommand extends Command {
+    private long token = new LoginUserService("nobody","").result();
+
     public MdCommand(Shell sh, String n) { super(sh, n); }
     public MdCommand(Shell sh, String n, String h) { super(sh, n, h); }
+
+    public long getToken(){
+        return token;
+    }
+
+    public void setToken(long token){
+        this.token = token;
+    }
 }
