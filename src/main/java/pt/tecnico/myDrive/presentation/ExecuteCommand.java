c15000a22f037d4297d578937e63ca2c02e2bde7
Radaeld <joao.serras@ist.utl.pt>
2016-04-30 22:32:18 +0100

    Execute File constructor implemented

--- src/main/java/pt/tecnico/myDrive/presentation/ExecuteCommand.java
+++ src/main/java/pt/tecnico/myDrive/presentation/ExecuteCommand.java
@@ -3,5 +3,10 @@ package pt.tecnico.myDrive.presentation;
 /**
  * Created by jp_s on 4/30/2016.
  */
-public class ExecuteCommand {
+public class ExecuteCommand extends MdCommand{
+
+    public ExecuteCommand(MdShell sh){super(sh, "do", "execute the file given by a path with the args");}
+
+   
 }
+
