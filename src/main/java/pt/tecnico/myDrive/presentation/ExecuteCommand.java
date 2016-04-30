3d5508e58a4b2e793f430b9dd5d1edbe696956dd
Radaeld <joao.serras@ist.utl.pt>
2016-04-30 23:05:18 +0100

    ExecuteFile Command function execute updated

--- src/main/java/pt/tecnico/myDrive/presentation/ExecuteCommand.java
+++ src/main/java/pt/tecnico/myDrive/presentation/ExecuteCommand.java
@@ -1,5 +1,7 @@
 package pt.tecnico.myDrive.presentation;
 
+import pt.tecnico.myDrive.service.ExecuteFileService;
+
 /**
  * Created by jp_s on 4/30/2016.
  */
@@ -8,6 +10,15 @@ public class ExecuteCommand extends MdCommand{
     public ExecuteCommand(MdShell sh){super(sh, "do", "execute the file given by a path with the args");}
 
     @Override
-    public void execute(String[] args) {}
+    public void execute(String[] args) {
+        String path = args[1];
+        String arg = "";
+        for(int i = 2; i < args.length; i++){
+            arg += " ";
+            arg += args[i];
+        }
+     //   ExecuteFileService executeFileService = new ExecuteFileService(((MdShell) shell()).getCurrentToken(), path, arg);
+     //   executeFileService.execute();
+    }
 }
 
