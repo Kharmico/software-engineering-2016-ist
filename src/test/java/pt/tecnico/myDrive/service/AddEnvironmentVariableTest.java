e267560d6796603521d35516964fc415d2009511
carinamgm <carinamartins@outlook.com>
2016-05-02 15:51:11 +0100

    addEnvVarTest fixed

--- src/test/java/pt/tecnico/myDrive/service/AddEnvironmentVariableTest.java
+++ src/test/java/pt/tecnico/myDrive/service/AddEnvironmentVariableTest.java
@@ -6,6 +6,7 @@ import pt.tecnico.myDrive.domain.MyDriveManager;
 import pt.tecnico.myDrive.domain.Session;
 import pt.tecnico.myDrive.exception.InvalidTokenException;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.LinkedHashMap;
@@ -22,48 +23,50 @@ public class AddEnvironmentVariableTest extends AbstractServiceTest {
 
 	    currentSession.setCurrentDir(mdm.getFilesystem().getHomeDirectory());
 	    mdm.addEnvironmentVariable("EnVarTest1", "SouO1", token);
-	    mdm.addEnvironmentVariable("EnVarTest2", "SouO2", token);	  
-	//    mdm.addEnvironmentVariable("EnvVarTest3", "Falhei", token);	//FIXME: is there any exception? or error?
+	    mdm.addEnvironmentVariable("EnVarTest2", "SouO2", token);	 
 	    
 	}
-	
-    private LinkedHashMap<String, String> getLinkedHashMap(long token) {
-        return MyDriveManager.getInstance().listEnvironmentVariables(token);
-    }
-    
-    private String getValue(long token, String value){
-    	return getLinkedHashMap(token).get(value);
-    }
-
 	
 	@Test
 	public void successNewEnvVar(){
 		String value = "/home/ItsMeMario";
-		String writtenValue = "";
+		LinkedHashMap<String, String> vars = new LinkedHashMap<String, String>();
+		
 		AddEnvironmentVariableService service = 
 				new AddEnvironmentVariableService(MyDriveManager.getInstance().getCurrentSession().getToken(), "IDoNotExistButIWill", value);
 		service.execute();
-		writtenValue = getValue(MyDriveManager.getInstance().getCurrentSession().getToken(), "IDoNotExistButIWill").toLowerCase();
+		vars = service.result();
+
+		assertTrue("Wrong size of vars", vars.size() == 3);
 		
-		assertTrue("Environment value was not created", writtenValue.contains("idonotexistbutiwill = " + value.toLowerCase()));	
+		assertEquals("Environment value was not created", vars.get("IDoNotExistButIWill"), value);
 	}
 
 	@Test
 	public void successExistingVar(){
-		String value = "NewValue";
-		String writtenValue = "";
-		AddEnvironmentVariableService service = 
-				new AddEnvironmentVariableService(MyDriveManager.getInstance().getCurrentSession().getToken(), "EnVarTest1", value);
-		service.execute();
-		writtenValue = getValue(MyDriveManager.getInstance().getCurrentSession().getToken(), "EnVarTest1").toLowerCase();
+		String value1 = "NewValue";
+		String value2 = "hi";
+		LinkedHashMap<String, String> vars = new LinkedHashMap<String, String>();
+
+		AddEnvironmentVariableService service1 = 
+				new AddEnvironmentVariableService(MyDriveManager.getInstance().getCurrentSession().getToken(), "EnVarTest1", value1);
+		service1.execute();
+		vars = service1.result();
 		
-		assertTrue("Environment value was not modified", writtenValue.contains("envartest1 = " + value.toLowerCase()));
+		AddEnvironmentVariableService service2 = 
+				new AddEnvironmentVariableService(MyDriveManager.getInstance().getCurrentSession().getToken(), "EnVarTest2", value2);
+		service2.execute();
+		vars.putAll(service2.result());
+		
+		assertTrue("Wrong size of vars", vars.size() == 2);
+		assertEquals("Environment value 1 was not modified", vars.get("EnVarTest1"), value1);
+		assertEquals("Environment value 2 was not modified", vars.get("EnVarTest2"), value2);
 	}
 	
 	
     @Test(expected = InvalidTokenException.class)
     public void invalidToken(){
-    	AddEnvironmentVariableService service = new AddEnvironmentVariableService(-1, "EnVarTest3", "t0k3n");
+    	AddEnvironmentVariableService service = new AddEnvironmentVariableService(-1, "EnVarTest2", "t0k3n");
     	service.execute();
     }
 	
