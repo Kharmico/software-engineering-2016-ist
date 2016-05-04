c1df9700fcfecb69c7502764e055ad4a8cd3f9d7
Radaeld <joao.serras@ist.utl.pt>
2016-05-04 12:21:48 +0100

    added new tests

--- src/test/java/pt/tecnico/myDrive/service/ExecuteFileTest.java
+++ src/test/java/pt/tecnico/myDrive/service/ExecuteFileTest.java
@@ -25,9 +25,18 @@ public class ExecuteFileTest extends AbstractServiceTest {
 
         manager.login("Pedrocas","Pedrocas");
         manager.createAppFile("sufix", "pt.tecnico.myDrive.MyDriveApplication.testAppfile");
+        manager.createAppFile("prefix", "pt.tecnico.myDrive.MyDriveApplication.testAppfile");
         manager.createAppFile("noMethod");
+        manager.createPlainFile("noPath");
+        manager.createPlainFile("SufixPath","/home/Pedrocas/sufix home Francisco Pedro");
+        manager.createPlainFile("SPPath","/home/Pedrocas/sufix\n/home/Pedrocas/sufix");
     }
-    
+
+    //////////////////////////////////////////
+    //           App File                   //
+    //////////////////////////////////////////
+
+
     @Test
     public void success(){
         String s = "home/Francisco/Pedro";
@@ -55,6 +64,40 @@ public class ExecuteFileTest extends AbstractServiceTest {
         String[] t = s.split("/");
         Directory d = (Directory) MyDriveManager.getInstance().getCurrentSession().getCurrentDir().getFather().getFileByName("Josefina");
         ExecuteFileService fl = new ExecuteFileService(d.getFileByName("noperm").getPath(),t, MyDriveManager.getInstance().getCurrentSession().getToken());
+        fl.execute();
+    }
+
+    //////////////////////////////////////////
+    //         Plain File                   //
+    //////////////////////////////////////////
+
+    /*
+    //Text PlainFIle with 2 paths
+    @Test
+    public void SucessPlainFile() {
+        String s = "home/Francisco/Pedro";
+        String[] t = s.split("/");
+        ExecuteFileService fl = new ExecuteFileService(MyDriveManager.getInstance().getCurrentSession().getCurrentDir().getFileByName("SPPath").getPath(),
+                t, MyDriveManager.getInstance().getCurrentSession().getToken());
+        fl.execute();
+    }*/
+
+    /*@Test
+    public void SucessPlainFile(){
+        String s = "home/Francisco/Pedro";
+        String[] t = s.split("/");
+        ExecuteFileService fl = new ExecuteFileService(MyDriveManager.getInstance().getCurrentSession().getCurrentDir().getFileByName("SufixPath").getPath(),
+                t, MyDriveManager.getInstance().getCurrentSession().getToken());
+        fl.execute();
+    }*/
+
+    // Teste para o caso de o metodo nao existir
+    @Test(expected = InvalidContentException.class)
+    public void noContetPlainFile(){
+        String s = "home/Francisco/Pedro";
+        String[] t = s.split("/");
+        ExecuteFileService fl = new ExecuteFileService(MyDriveManager.getInstance().getCurrentSession().getCurrentDir().getFileByName("noMethod").getPath(),
+                t, MyDriveManager.getInstance().getCurrentSession().getToken());
         fl.execute();
     }
 
