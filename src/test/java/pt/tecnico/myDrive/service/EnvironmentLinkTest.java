8b78f99f4f0df8ef51227c06242a103a5a49c524
Radaeld <joao.serras@ist.utl.pt>
2016-05-05 12:12:10 +0100

    added tests

--- src/test/java/pt/tecnico/myDrive/service/EnvironmentLinkTest.java
+++ src/test/java/pt/tecnico/myDrive/service/EnvironmentLinkTest.java
@@ -1,7 +1,102 @@
 package pt.tecnico.myDrive.service;
 
-/**
- * Created by jp_s on 5/3/2016.
- */
-public class EnvironmentLinkTest {
+
+import mockit.Expectations;
+import mockit.Injectable;
+import mockit.integration.junit4.JMockit;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import pt.tecnico.myDrive.domain.Directory;
+import pt.tecnico.myDrive.domain.LinkFile;
+import pt.tecnico.myDrive.domain.MyDriveManager;
+import pt.tecnico.myDrive.domain.User;
+import pt.tecnico.myDrive.exception.AccessDeniedException;
+import pt.tecnico.myDrive.exception.FileUnknownException;
+import sun.awt.image.ImageWatched;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(JMockit.class)
+public class EnvironmentLinkTest extends AbstractServiceTest {
+
+    @Injectable
+    private LinkFile fl;
+    private LinkFile file;
+
+    @Override
+    public void populate() {
+        MyDriveManager manager = MyDriveManager.getInstance();
+        manager.addUser("Francisco");
+        manager.addUser("Alberto95");
+
+        manager.login("Alberto95","Alberto95");
+        manager.addEnvironmentVariable("Cenoura", "Exame", manager.getCurrentSession().getToken());
+        manager.createDirectory("Exame");
+        manager.getCurrentSession().setCurrentDir((Directory)manager.getCurrentSession().getCurrentDir().getFileByName("Exame"));
+        manager.createPlainFile("prefix.txt","Engenharia dos sistemas");
+        manager.createLinkFile("PrimeiroExame", "/home/Alberto95/$Cenoura/prefix.txt");
+        file = (LinkFile) MyDriveManager.getInstance().getCurrentSession().getCurrentDir().getFileByName("PrimeiroExame");
+
+
+        manager.login("Francisco","Francisco");
+        manager.addEnvironmentVariable("Batata", "teste", manager.getCurrentSession().getToken());
+
+        manager.createDirectory("teste");
+        manager.getCurrentSession().setCurrentDir((Directory)manager.getCurrentSession().getCurrentDir().getFileByName("teste"));
+        manager.createPlainFile("sufix.txt","Engenharia dos sistemas de info");
+        manager.getCurrentSession().setCurrentDir(manager.getCurrentSession().getCurrentDir().getFather());
+        manager.createLinkFile("PrimeiroTeste", "/home/Francisco/$Batata/sufix.txt");
+        manager.createLinkFile("SegundoTeste", "/home/Francisco/$Alface/sufix.txt");
+        fl = (LinkFile) MyDriveManager.getInstance().getCurrentSession().getCurrentDir().getFileByName("PrimeiroTeste");
+    }
+
+    private User getUser(){
+
+        return MyDriveManager.getInstance().getCurrentSession().getCurrentUser();
+    }
+
+
+    @Test
+    public void PrintSucess(){
+        new Expectations(LinkFile.class) {
+            {
+                fl.getContent();
+                result = "/home/Francisco/teste/sufix.txt";
+            }
+        };
+        assertEquals("Wrong content","Engenharia dos sistemas de info",fl.printContent(getUser()));
+    }
+
+    //Directory Doesnt Exist
+    @Test(expected = FileUnknownException.class)
+    public void wrongPath(){
+        new Expectations(LinkFile.class) {
+            {
+                fl.getContent();
+                result = "/home/Alberto95/teste/sufix.txt";
+            }
+        };
+        fl.printContent(getUser());
+    }
+
+    //Directory Doesnt Exist
+    @Test(expected = FileUnknownException.class)
+    public void nonExistingVarPath(){
+        fl.printContent(getUser());
+    }
+
+    //Doenst have permission Exist
+    @Test(expected = AccessDeniedException.class)
+    public void permissionDeniedExist(){
+        new Expectations(LinkFile.class) {
+            {
+                file.getContent();
+                result = "/home/Alberto95/Exame/prefix.txt";
+            }
+        };
+        file.printContent(getUser());
+    }
+
+
+
 }
--- src/test/java/pt/tecnico/myDrive/service/ExecuteFileTest.java
+++ src/test/java/pt/tecnico/myDrive/service/ExecuteFileTest.java
@@ -3,16 +3,25 @@ package pt.tecnico.myDrive.service;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import pt.tecnico.myDrive.domain.Directory;
 import pt.tecnico.myDrive.domain.MyDriveManager;
 import pt.tecnico.myDrive.exception.AccessDeniedException;
 import pt.tecnico.myDrive.exception.InvalidContentException;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
 
 public class ExecuteFileTest extends AbstractServiceTest {
 
     private static final Logger log = LogManager.getRootLogger();
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private PrintStream ps;
 
     @Override
     public void populate() {
@@ -32,19 +41,36 @@ public class ExecuteFileTest extends AbstractServiceTest {
         manager.createPlainFile("SPPath","/home/Pedrocas/sufix\n/home/Pedrocas/sufix");
     }
 
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        ps  =  System.out;
+
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.out.flush();
+        System.setOut(ps);
+    }
 
     //////////////////////////////////////////
     //           App File                   //
     //////////////////////////////////////////
 
     @Test
-    public void success(){
-        String[] t = {"cd","ls"};
+    public void success() {
+        String[] t = {"cd", "ls"};
+
         ExecuteFileService fl = new ExecuteFileService(MyDriveManager.getInstance().getCurrentSession().getCurrentDir().getFileByName("sufix").getPath(),
                 t, MyDriveManager.getInstance().getCurrentSession().getToken());
         fl.execute();
-     //   assertEquals("App file isnt working","AppFileRunning");
+
+        String[] output = outContent.toString().split("\n");
+
+        assertEquals("App file isnt working", "AppFileRunning", output[1]);
     }
+
 
     // Teste para o caso de o metodo nao existir
     @Test(expected = InvalidContentException.class)
@@ -68,22 +94,32 @@ public class ExecuteFileTest extends AbstractServiceTest {
     //         Plain File                   //
     //////////////////////////////////////////
 
-    /*
+/*
     //Text PlainFIle with 2 paths
     @Test
-    public void SucessPlainFile() {
+    public void SucessPlainFile2path() {
         String[] t = {"cd","ls"};
         ExecuteFileService fl = new ExecuteFileService(MyDriveManager.getInstance().getCurrentSession().getCurrentDir().getFileByName("SPPath").getPath(),
                 t, MyDriveManager.getInstance().getCurrentSession().getToken());
         fl.execute();
-    }*/
 
-    /*@Test
-    public void SucessPlainFile(){
+        String[] output = outContent.toString().split("\n");
+
+        assertEquals("App file isnt working", "AppFileRunning", output[1]);
+    }
+
+
+    //Text PlainFIle with 1 paths
+    @Test
+    public void SucessPlainFile1path(){
         String[] t = {"cd","ls"};
         ExecuteFileService fl = new ExecuteFileService(MyDriveManager.getInstance().getCurrentSession().getCurrentDir().getFileByName("SufixPath").getPath(),
                 t, MyDriveManager.getInstance().getCurrentSession().getToken());
         fl.execute();
+
+        String[] output = outContent.toString().split("\n");
+
+        assertEquals("App file isnt working", "AppFileRunning", output[1]);
     }*/
 
     // Teste para o caso de o metodo nao existir
